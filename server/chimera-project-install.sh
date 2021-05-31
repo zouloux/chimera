d101fa074a1ef21d1218d37813bf94cfc95750d3
Alexis Bouhet <alex@zouloux.com>
2021-05-31 18:43:39 +0200

    Patched project dot env generation

--- server/chimera-project-install.sh
+++ server/chimera-project-install.sh
@@ -3,4 +3,4 @@ cd "$1"
 mv $2 docker-compose.yaml > /dev/null 2>&1
 # FIXME : Do this only not already existing in file
 echo "" >> .env
-echo "CHIMERA_HOST_PREFIX=$3." >> .env
+echo "CHIMERA_HOST_PREFIX=$3" >> .env
