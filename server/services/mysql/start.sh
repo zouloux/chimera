77066d05a7672ea0bc744ee353474b951f5a4659
Alexis Bouhet <alex@zouloux.com>
2021-07-06 18:06:02 +0200

    New mysql start script

--- server/services/mysql/start.sh
+++ server/services/mysql/start.sh
@@ -3,13 +3,13 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-if [ ! -f services/mysql/.env ]; then
+if [ ! -f .env ]; then
   LOCAL_MARIA_PASSWORD=$(cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 10)
-  echo "MYSQL_ROOT_PASSWORD=${LOCAL_MARIA_PASSWORD}" > services/mysql/.env
+  echo "MYSQL_ROOT_PASSWORD=${LOCAL_MARIA_PASSWORD}" > .env
 
   echo "A ${bold}new password for MariaDB${normal} has been created :"
   echo "→ ${bold}${LOCAL_MARIA_PASSWORD}${normal}"
-  echo "( This password is visible in ${bold}$(pwd)/services/mysql/.env${normal} anyway )"
+  echo "( This password is visible in ${bold}$(pwd)chimera/server/services/mysql/.env${normal} anyway )"
   echo ""
   read -s -n 1 -p "Continue ..."
   echo "";echo ""
