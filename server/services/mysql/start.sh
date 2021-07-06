44ba3e7610ab1b7d1d255999475fdc5a5a1638fa
Alexis Bouhet <alex@zouloux.com>
2021-07-06 18:08:28 +0200

    New mysql start script

--- server/services/mysql/start.sh
+++ server/services/mysql/start.sh
@@ -7,21 +7,25 @@ if [ ! -f .env ]; then
   LOCAL_MARIA_PASSWORD=$(cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 10)
   echo "MYSQL_ROOT_PASSWORD=${LOCAL_MARIA_PASSWORD}" > .env
 
+  echo "${bold}MariaDB setup${normal}"
+  echo ""
   echo "A ${bold}new password for MariaDB${normal} has been created :"
   echo "→ ${bold}${LOCAL_MARIA_PASSWORD}${normal}"
   echo "( This password is visible in ${bold}$(pwd)chimera/server/services/mysql/.env${normal} anyway )"
   echo ""
-  read -s -n 1 -p "Continue ..."
-  echo "";echo ""
-  echo "MariaDB will be started. Connection instructions :"
+  echo "Press any key to continue ..."
+  read -s -n 1
+  echo "";
+  echo "Connection instructions :"
   echo "- host (from localhost) : 127.0.0.1"
   echo "- host (from docker): maria"
   echo "- user : root"
   echo "- password : ${LOCAL_MARIA_PASSWORD}"
   echo "- port : 3306"
   echo ""
-  read -s -n 1 -p "Continue ..."
-  echo "";echo ""
+  echo "Press any key to continue ..."
+  read -s -n 1
+  echo "";
 fi
 
 echo "PhpMyAdmin is available here : "
