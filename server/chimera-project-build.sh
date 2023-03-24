68cd4ee10b420f59460900a96b7765d6bee7333b
Alexis Bouhet <alexis@les-hallucines.studio>
2023-03-24 15:40:41 +0100

    Updated docker-compose to docker composer in chimera scripts

--- server/chimera-project-build.sh
+++ server/chimera-project-build.sh
@@ -2,4 +2,4 @@
 cd "$1"
 
 # Try to build with cache, if it failed, try to rebuild from scratch
-docker-compose build || docker-compose build --no-cache
+docker compose build || docker compose build --no-cache
